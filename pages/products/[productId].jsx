1a747bdf7aec0b979c96323531ee300146839007
unseoJang <qqbck123@naver.com>
2024-08-11 23:55:05 +0900

    getServerSideProps 와 useEffect 데이터 호풀 흐름 비교

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,3 +1,5 @@
+import { ProductHeader } from '@/components/ProductHeader';
+import axios from 'axios';
 import React from 'react';
 
 /**
@@ -5,14 +7,23 @@ import React from 'react';
  * @function ProductDetailPage
  **/
 
-const ProductDetailPage = ({ message }) => {
-	return <div>ProductDetailPage : {message}</div>;
+const ProductDetailPage = ({ message, productInfo }) => {
+	const headerTitle = '상품 상세 정보 페이지';
+	return (
+		<div>
+			<ProductHeader title={headerTitle} />
+			<div>ProductDetailPage : {message}</div>
+			<p>{productInfo.name}</p>
+		</div>
+	);
 };
 
 export async function getServerSideProps(context) {
-	console.log(context.params.productId);
+	// console.log(context.params.productId);
+	const id = context.params.productId;
+	const response = await axios.get(`http://localhost:4000/products/${id}`);
 	return {
-		props: { message: '서버에서 보내준 메시지' },
+		props: { message: '서버에서 보내준 메시지', productInfo: response.data },
 	};
 }
 
